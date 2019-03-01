c30dbd328cb5ab3c2c50b06d9b5cf5a041e47eab
limsammy <limsammy1@gmail.com>
2019-03-01 13:55:03 -0800

    Uploaded dataset to gcloud bucket and edited permissions so this script can access it

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@
 
 import os
 import requests
+import urllib.request
 
+TRAIN_TAR_URL = 'https://storage.googleapis.com/weap-detect-bucket/datasets/train.tar.gz'
+TEST_TAR_URL = 'https://storage.googleapis.com/weap-detect-bucket/datasets/test.tar.gz'
 TRAIN_PATH = 'images/train'
 TEST_PATH = 'images/test'
 
 def create_dir(path):
 	if not os.path.exists(path):
 		os.makedirs(path)
+
 
